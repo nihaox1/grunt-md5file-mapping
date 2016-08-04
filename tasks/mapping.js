b8cd9d23446f160dd554ff73e7824c1485c5e5c5
bo.cheng <chengbo@QXIT-PC-000106.local>
2016-08-04 19:28:54 +0800

    update

--- tasks/mapping.js
+++ tasks/mapping.js
@@ -1,15 +1,5 @@
 var MD5File = require( "md5-file" ),
     Path    = require( "path" );
- 
-// md5File('LICENSE.md', (err, hash) => {
-//   if (err) throw err
- 
-//   console.log(`The MD5 sum of LICENSE.md is: ${hash}`)
-// })
- 
-// /* Sync usage */
-// const hash = md5File.sync('LICENSE.md')
-// console.log(`The MD5 sum of LICENSE.md is: ${hash}`)
 
 module.exports = function( grunt ) {
     grunt.registerMultiTask( "md5fileMapping" , "mapping file" , function() {
@@ -19,13 +9,13 @@ module.exports = function( grunt ) {
         grunt.file.recurse( Path.join( _self.data.folderSrc ) , function( filePath , folder , d , fileName ) {
             var _hash,
                 _fileName;
-            if( _fileExec.test( fileName ) ) {
+            if( filePath.replace( _self.data.folderSrc , "").replace( /^[\\|\/]/ , "").split( /[\\|\/]/).length == 1 && _fileExec.test( fileName ) ) {
                 _hash    = MD5File.sync( filePath );
                 _fileName= fileName.replace( /(.*)\.js/ , "$1." + _hash + ".js" );
                 _mappingHash[ fileName ]    = _fileName;
                 grunt.file.write( Path.join( _self.data.folderDist , _fileName ) , grunt.file.read( filePath ) );
             }
         });
-        grunt.file.write( Path.join( _self.data.dist ) , JSON.stringify( _mappingHash ) );
+        grunt.file.write( Path.join( _self.data.dist ) , JSON.stringify( _mappingHash , null , "    " ) );
     });
 };
